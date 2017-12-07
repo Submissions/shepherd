aa4111e5065a175f6cc7468040a259e29458b6db
Evette Skinner <skinner.en@gmail.com>
2017-12-07 16:23:46 -0600

    created script and defined command-line arguments

---
+++ send_batch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+project_name = sys.argv[1]
+phase = sys.argv[2]
+subproject_name = sys.argv[3]
+batch_group = sys.argv[4]
+batch_name = sys.argv[5]
+batch_input_file = sys.argv[6]
+globus_input_file = sys.argv[7]
+
