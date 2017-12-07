2e92bab3a4f5327f356ad8b36fdfc3ae20b8639d
Evette Skinner <skinner.en@gmail.com>
2017-12-07 17:16:13 -0600

    created batch directory

--- send_batch.py
+++ send_batch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 
+import os
 import sys
+import subprocess
 
 project_name = sys.argv[1]
 phase = sys.argv[2]
@@ -10,3 +12,4 @@ batch_name = sys.argv[5]
 batch_input_file = sys.argv[6]
 globus_input_file = sys.argv[7]
 
+subprocess.Popen(['mkdir', '-p','project_name/phase/subproject_name/batch_group/batch_name'])
