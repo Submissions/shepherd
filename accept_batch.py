82164b2aa6c678cec83e3c67f81bc71496e909e5
Christian Sanchez <cssanchez23gmail.com>
2017-12-21 13:24:17 -0600

    also added the meta.yaml to work with

--- accept_batch.py
+++ accept_batch.py
@@ -11,23 +11,43 @@ import yaml
 /stornext/submissions/topmed/md5-batches/CARDIA_batch17a/meta.yaml
 /stornext/submissions/topmed/validation-batches/CARDIA_batch17a/meta.yaml
 """
-input_file= sys.argv[1]
-input_path= Path(input_file).absolute()
+input_path= Path(sys.argv[1])
+input_path.glob('meta.{yaml,yml,txt}')
+meta_hits = list(input_path.glob('meta.*'))
+assert len(meta_hits) == 1, meta_hits
+meta_path = meta_hits[0]
+meta_doc = yaml.load(meta_path.read_text())
+
+class Generic:
+    pass
+meta = Generic()
+meta.__dict__.update(meta_doc)
+
+bname = meta.batch_title[7:]
+#print (bname)
+
+#input_path= Path(input_file).absolute()
 base_dirr =Path('')
 base_dir = base_dirr.absolute()
 print (base_dir)
+
 meta_base = Path('/groups/submissions/metadata/v1/topmed')
 sub_base = Path('/stornext/snfs1/submissions/topmed')
-meta = "meta.yaml"
+asp_base = Path(' christis@hgsc-aspera1.hgsc.bcm.edu:/share/share/globusupload/submissions')
+
+
 
 def write_yaml(path):
     "This bad boy writes yaml files"
+    meta = "meta.yaml"
     file_create= path / meta
     with file_create.open("w", encoding ="utf-8") as f:
         f.write("yaml /n")
         return
 
+"""
     
+
 with open(input_file) as file:
     input=yaml.load(file)
 
@@ -37,6 +57,8 @@ meta_test = input['Resources']['MyEC2Instance']['Properties']['KeyName']
 
 #below makes directory will change "test" for the variable 
 #below makes files in path I am writing
+asp_dir = asp_base / test_path
+asp_dir.mkdir()
 test_dir = base_dir / test_path
 if test_dir.exists():
     
@@ -70,6 +92,6 @@ if valid.exists():
 
 else:
     valid.mkdir()
-
+"""
 
 
