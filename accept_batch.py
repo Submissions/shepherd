80cc4f3d4cd9c7a4abccdc3d269b3355ba15f481
Christian Sanchez <cssanchez23gmail.com>
2018-01-05 11:29:03 -0600

    WIP

--- accept_batch.py
+++ accept_batch.py
@@ -5,12 +5,19 @@ import os
 import sys
 import yaml
 """
+Arguement will be path where meta file is found
+
 /groups/submissions/metadata/v1/topmed/phase3/cardia/01/17a/{md5,validation}
 /groups/submissions/metadata/v1/topmed/phase3/cardia/01/17a/state/{00,current}.yaml
 /aspera/share/globusupload/submissions/cardia/CARDIA_batch17a/meta.yaml
 /stornext/submissions/topmed/md5-batches/CARDIA_batch17a/meta.yaml
 /stornext/submissions/topmed/validation-batches/CARDIA_batch17a/meta.yaml
 """
+sub_base = Path('/stornext/snfs1/submissions/topmed')
+asp_base = Path(' christis@hgsc-aspera1.hgsc.bcm.edu:/share/share/globusupload/submissions')
+g_base = Path('/groups/submissions/metadata/v1')
+base_dirr =Path('')
+base_dir = base_dirr.absolute()
 input_path= Path(sys.argv[1])
 input_path.glob('meta.{yaml,yml,txt}')
 meta_hits = list(input_path.glob('meta.*'))
@@ -18,45 +25,58 @@ assert len(meta_hits) == 1, meta_hits
 meta_path = meta_hits[0]
 meta_doc = yaml.load(meta_path.read_text())
 
+
 class Generic:
     pass
 meta = Generic()
 meta.__dict__.update(meta_doc)
 
-bname = meta.batch_title[7:]
-#print (bname)
-
-#input_path= Path(input_file).absolute()
-base_dirr =Path('')
-base_dir = base_dirr.absolute()
-print (base_dir)
-
-meta_base = Path('/groups/submissions/metadata/v1/topmed')
-sub_base = Path('/stornext/snfs1/submissions/topmed')
-asp_base = Path(' christis@hgsc-aspera1.hgsc.bcm.edu:/share/share/globusupload/submissions')
-
+def is_cram(meta):
+    "This bad boy ensures file format is just cram"
+    ft = meta.file_formats[0]
+    assert ft.lower() == "cram"
 
 
 def write_yaml(path):
     "This bad boy writes yaml files"
-    meta = "meta.yaml"
-    file_create= path / meta
+    meta_p = "meta.yaml"
+    file_create= path / meta_p
     with file_create.open("w", encoding ="utf-8") as f:
         f.write("yaml /n")
         return
 
-"""
+def check_or_make(path):
+    if path.exist():
+        print ("%s  exist")% path
+    else:
+        path.mkdir()
+
+def make_paths(input_p):
+    md5_g = Path(input_p,"md5")
+    check_or_make(md5_g)
+
     
 
-with open(input_file) as file:
-    input=yaml.load(file)
+batch_name = meta.batch_title[7:]
+batch_num = meta.batch_tittle
+sub_proj = meta.batch_title[7:-9]
+project_code = meta.project_code
+samp_num = meta.num_records
 
-test_path = input['Resources']['MyEC2Instance']['Type']
-meta_test = input['Resources']['MyEC2Instance']['Properties']['KeyName']
+print (sub_proj)
+make_paths(input_path)
+
+dest_path = Path(g_base, *input_path.parts[-5:])
+
+print (dest_path)
+dest_path.mkdir(parents=True, exist_ok=True)
+write_yaml(dest_path)
 
 
+#test_path = input['Resources']['MyEC2Instance']['Type']
 #below makes directory will change "test" for the variable 
 #below makes files in path I am writing
+"""
 asp_dir = asp_base / test_path
 asp_dir.mkdir()
 test_dir = base_dir / test_path
@@ -93,5 +113,4 @@ if valid.exists():
 else:
     valid.mkdir()
 """
-
 
