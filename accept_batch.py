d06050b555438d44317cbc56a20dfa8fd4123270
Christian Sanchez <cssanchez23gmail.com>
2018-01-05 13:59:59 -0600

    cleaned the code..removed unnessary lines

--- accept_batch.py
+++ accept_batch.py
@@ -46,71 +46,34 @@ def write_yaml(path):
         return
 
 def check_or_make(path):
-    if path.exist():
-        print ("%s  exist")% path
+    if path.exists():
+        print ("%s exist" % (path)) 
     else:
         path.mkdir()
 
 def make_paths(input_p):
     md5_g = Path(input_p,"md5")
     check_or_make(md5_g)
-
+    val_g = Path(input_p,"validation")
+    check_or_make(val_g)
     
 
 batch_name = meta.batch_title[7:]
-batch_num = meta.batch_tittle
+batch_num = meta.batch_title[-3:-1]
 sub_proj = meta.batch_title[7:-9]
 project_code = meta.project_code
 samp_num = meta.num_records
 
-print (sub_proj)
+
 make_paths(input_path)
 
 dest_path = Path(g_base, *input_path.parts[-5:])
 
-print (dest_path)
+
 dest_path.mkdir(parents=True, exist_ok=True)
 write_yaml(dest_path)
 
 
-#test_path = input['Resources']['MyEC2Instance']['Type']
-#below makes directory will change "test" for the variable 
-#below makes files in path I am writing
-"""
-asp_dir = asp_base / test_path
-asp_dir.mkdir()
-test_dir = base_dir / test_path
-if test_dir.exists():
-    
-    print (base_dir)
-    print ("base_dir above")
-    
-    write_yaml(test_dir)
-    
-else:
-    test_dir.mkdir()
-    write_yaml(test_dir)
 
 
-meta_dir = meta_base / meta_test
-if meta_dir.exists():
-    print ("meta exist")
-
-else:
-    meta_dir.mkdir()
-
-md5 = sub_base /"md5-batches"/meta_test
-valid = sub_base /"validation-batches"/meta_test
-if md5.exists():
-    print ("md5 exist")
-
-else:
-    md5.mkdir()
-
-if valid.exists():
-    print ("valid exist")
-
-else:
-    valid.mkdir()
-"""
 
