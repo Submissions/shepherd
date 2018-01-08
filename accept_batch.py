f0f16b65806234397eb848af36847b9fbb2a7c66
Christian Sanchez <cssanchez23gmail.com>
2018-01-08 15:30:51 -0600

    cleaned the code

--- accept_batch.py
+++ accept_batch.py
@@ -7,16 +7,12 @@ import yaml
 """
 Arguement will be path where meta file is found
 need to run in prod, since will call aspera
-/groups/submissions/metadata/v1/topmed/phase3/cardia/01/17a/{md5,validation}
-/groups/submissions/metadata/v1/topmed/phase3/cardia/01/17a/state/{00,current}.yaml
-/aspera/share/globusupload/submissions/cardia/CARDIA_batch17a/meta.yaml
-/stornext/submissions/topmed/md5-batches/CARDIA_batch17a/meta.yaml
-/stornext/submissions/topmed/validation-batches/CARDIA_batch17a/meta.yaml
 """
 sub_base = Path('/stornext/snfs1/submissions/topmed/test')
 #asp_base = Path('/aspera/share/share/globusupload/submissions/test') #Might need to run in prod
 asp_base = Path('aspera/share/globusupload/submissions')
 g_base = Path('/groups/submissions/metadata/v1')
+dest_path = Path(g_base, *input_path.parts[-5:])
 base_dirr =Path('')
 base_dir = base_dirr.absolute()
 input_path= Path(sys.argv[1])
@@ -37,13 +33,17 @@ def is_cram(meta):
     ft = meta.file_formats[0]
     assert ft.lower() == "cram"
 
-
 def write_yaml(path):
     "This bad boy writes yaml files"
     meta_p = "meta.yaml"
     file_create= path / meta_p
     with file_create.open("w", encoding ="utf-8") as f:
-        f.write("yaml /n")
+        f.write("""state_id: 0 
+      steps_completed: 0 
+      state: 
+        \t copy: initial 
+        \t md5: initial 
+        \t validation: initial \n""")
         return
 
 def check_or_make(path):
@@ -53,6 +53,7 @@ def check_or_make(path):
     else:
         path.mkdir(parents=True)
         write_yaml(path)
+
 def make_paths(input_p):
     md5_g = Path(input_p,"md5")
     check_or_make(md5_g)
@@ -65,21 +66,19 @@ def make_paths(input_p):
     check_or_make(md5_s)
     check_or_make(val_s)
 
-
 batch_name = meta.batch_title[7:]
 batch_num = meta.batch_title[-3:-1]
 sub_proj = meta.batch_title[7:-9]
 project_code = meta.project_code
 samp_num = meta.num_records
 
-
 make_paths(input_path)
 
-dest_path = Path(g_base, *input_path.parts[-5:])
 
 
-#dest_path.mkdir(parents=True, exist_ok=True)
-#write_yaml(dest_path)
+
+
+
 
 
 
