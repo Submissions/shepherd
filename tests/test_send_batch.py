72c6f73f1ca849ca9b4b29cc54b907b019186e1a
Walker Hale IV <walker.hale.iv@gmail.com>
2018-01-10 15:05:07 -0600

    Expand test fixture

--- tests/test_send_batch.py
+++ tests/test_send_batch.py
@@ -1,14 +1,30 @@
 from pytest import fixture
+import yaml
 
 
-def test_a(send_batch):
-    assert 0, send_batch
+def test_a(send_batch_fixture):
+    print(send_batch_fixture.root_dir.listdir())
+    with open(send_batch_fixture.root_dir.join('config.yaml')) as fin:
+        print(yaml.load(fin))
+    assert 0, vars(send_batch_fixture)
 
 
-def test_b(send_batch):
-    assert 0, send_batch
+def test_b(send_batch_fixture):
+    assert 0, send_batch_fixture
 
 
 @fixture(scope='module')
-def send_batch(tmpdir_factory):
-    return tmpdir_factory.mktemp('send_batch')
+def send_batch_fixture(tmpdir_factory):
+    return SendBatchFixture(tmpdir_factory)
+
+
+class SendBatchFixture:
+    def __init__(self, tmpdir_factory):
+        self.root_dir = tmpdir_factory.mktemp('send_batch')
+        self.pm_root = self.root_dir.join('pm_root')
+        self.sub_root = self.root_dir.join('sub_root')
+        self.config_file = self.root_dir.join('config.yaml')
+        config = dict(pm_root=str(self.pm_root), sub_root=str(self.sub_root))
+        self.config_file.write_text(
+            yaml.dump(config, default_flow_style=False), 'ascii'
+        )
