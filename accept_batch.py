eeee85adc1f821bd9c0ea2d7237791e5f752ea04
Marcela Torres <itsmarcy@gmail.com>
2019-06-12 13:27:45 -0500

    Update accept_batch
    - Returns an exit code of 0 or 1 depending on status. This is
    apart of the effort to automate the whole process from another
    script.
    - Added a method that checks if the given TSV file exist, if not
    to mark the acceptance as a failure.
    - Reorganized code.

--- accept_batch.py
+++ accept_batch.py
@@ -1,112 +1,131 @@
-from pathlib import Path
+#!/usr/bin/env python3
+
 import argparse
 import logging
-import os
-import subprocess
 import sys
 import yaml
+from pathlib import Path
 
 from config import get_config
 
 
-config = get_config()
-sub_root = config.sub_root
-asp_root = config.asp_root
+def main():
+    args = parse_args()
+    configure_logging()
+    run(args.working_path)
+    sys.exit(0)
 
-"""
-Arguement will be path where meta file is found
-need to run in prod, since will call aspera
-"""
-#sub_base = Path('/stornext/snfs1/submissions/topmed')
-#sub base will be defined in config now
-g_base = Path(sub_root)
-input_path= Path(sys.argv[1])
-dest_path = Path(g_base, *input_path.parts[-5:])
 
-input_path.glob('meta.{yaml,yml,txt}')
-meta_hits = list(input_path.glob('meta.*'))
-assert len(meta_hits) == 1,"There are too many meta hits"
-meta_path = meta_hits[0]
-meta_doc = yaml.load(meta_path.read_text())
-#assert type(name) is StringType, "name is not a string: %r" % name
+def configure_logging():
+    global logger
+    err_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    err_handler.setFormatter(formatter)
+    logger = logging.getLogger("shepherd.accept_batch")
+    logger.addHandler(err_handler)
+    logger.setLevel(logging.INFO)
 
-logging.basicConfig(level=logging.INFO)
-class Generic:
-    pass
-meta = Generic()
-meta.__dict__.update(meta_doc)
 
-def print_tsv(tsv_file_path_str):
-    spreadsheet_path=Path(tsv_file_path_str)
-    sname = spreadsheet_path.name
-    
-    if input_file == sname:
-        logging.info('TSV file: %s' % (sname))
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("working_path")
+    args = parser.parse_args()
+    return args
+
+
+def run(input_path):
+    # Get config paths
+    config = get_config()
+    sub_root = Path(config.sub_root)
+    asp_root = config.asp_root
+    input_path = Path(input_path)
+    dest_path = Path(sub_root, *input_path.parts[-5:])
+
+    # Get meta info
+    meta_path = get_meta_file(input_path)
+    meta = dict(yaml.safe_load(meta_path.read_text()))
+
+    try:
+        batch_name = meta["batch_title"][7:]
+        batch_num = meta["batch_title"][-3:-1]
+        sub_proj = meta["batch_title"][7:-9]
+        project_code = meta["project_code"]
+        samp_num = meta["num_records"]
+        input_file = meta["input"]
+    except KeyError:
+        logger.exception("Missing key from meta file.")
+        sys.exit(1)
+
+    # Check that the tsv file exist
+    if not Path(input_path, input_file).exists():
+        logger.error(f"TSV file {input_file} does not exist.")
+        sys.exit(1)
+
+    # Check thats the file is a CRAM
+    if is_cram(meta["file_formats"]):
+        make_paths(asp_root, batch_name, dest_path, sub_proj)
+
+    logger.info("PROJECT CODE: %s" % (project_code))
+    logger.info("FILE NAME: %s" % (meta_path.name))
+    logger.info("Batch successfully accepted.")
+
+
+def get_meta_file(input_path):
+    meta_hits = list(input_path.glob("meta.*"))
+    if len(meta_hits) > 1:
+        logger.error("There are too many meta hits.")
+        sys.exit(1)
+    elif len(meta_hits) == 0:
+        logger.error("No meta hits were found.")
+        sys.exit(1)
     else:
-        logging.warning('TSV file name in meta and in path do not match')                           
-def is_cram(meta):
-    "This bad boy ensures file format is just cram"
-    ft = meta.file_formats[0]
-    if ft.lower() == "cram":
-        return True
-    else:
-        return False
+        return meta_hits[0]
 
-def write_yaml(path):
-    "This bad boy writes yaml files"
-    meta_p = "00.yaml"
-    file_create= path / meta_p
-    with file_create.open("w", encoding ="utf-8") as f:
-        print("""state_id: 0
-steps_completed: 0
-state:
-  copy: initial
-  md5: initial
-  validation: initial""", file=f)
-        return
 
-def check_or_make(path):
-    if path.exists():
-        logging.warning("%s exist" % (path)) 
-    else:
-        path.mkdir(parents=True)
+def is_cram(file_formats):
+    """This bad boy ensures file format is just cram"""
+    for file in file_formats:
+        if file.lower() != "cram":
+            logger.error("This is not a CRAM!")
+            sys.exit(1)
+    return True
 
-def aspera_path(input_p):
-    cmd="scp -r "+ "meta.yaml" +" "+ aspd_base+"/"+batch_name+"/"
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print (cmd)
 
-def make_paths(input_path, dest_path):
-    md5_path = Path(dest_path, 'md5')
-    validation_path = Path(dest_path, 'validation')
-    state_path = Path(dest_path, 'state')
+def make_paths(asp_root, batch_name, dest_path, sub_proj):
+    md5_path = Path(dest_path, "md5")
+    validation_path = Path(dest_path, "validation")
+    state_path = Path(dest_path, "state")
     check_or_make(md5_path)
     check_or_make(validation_path)
     check_or_make(state_path)
     write_yaml(state_path)
-    link_current_path = state_path / 'current.yaml'
-    link_current_path.symlink_to('00.yaml')
-    aspera_root = Path(asp_root,sub_proj,batch_name)
+    link_current_path = state_path / "current.yaml"
+    link_current_path.symlink_to("00.yaml")
+    aspera_root = Path(asp_root, sub_proj, batch_name)
     check_or_make(aspera_root)
 
 
-batch_name = meta.batch_title[7:]
-batch_num = meta.batch_title[-3:-1]
-sub_proj = meta.batch_title[7:-9]
-project_code = meta.project_code
-samp_num = meta.num_records
-input_file = meta.input
-
-if is_cram(meta) == True:
-    #logging.info("This is a cram")
-    make_paths(input_path, dest_path)
-else:
-    logging.error("!!Not a Cram!!!")
+def check_or_make(path):
+    if path.exists():
+        logger.warning("Path %s exist" % (path))
+        sys.exit(1)
+    else:
+        path.mkdir(parents=True)
 
 
-print_tsv(meta.input)
-logging.info("PROJECT CODE: %s" % (project_code))
-logging.info("FILE NAME: %s" % (meta_path.name))
+def write_yaml(path):
+    """This bad boy writes yaml files"""
+    file_create = path / "00.yaml"
+    data = {
+        "state_id": 0,
+        "steps_completed": 0,
+        "state": {"copy": "initial", "md5": "initial", "validation": "initial"},
+    }
+    with open(file_create, "w") as outfile:
+        yaml.dump(data, outfile, sort_keys=False)
 
 
+if __name__ == "__main__":
+    main()
